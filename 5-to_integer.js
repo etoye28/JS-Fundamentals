101a2b199bb03d7c41e2349cf7fa6309a12aca77
Etsub Berihun <etsubberihun09@gmail.com>
2025-07-11 23:00:22 +0300

    Task 5

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,7 @@
 const arg1= process.argv[2];
 let c = parseInt(arg1);
-if (c){
-    console.log("My number"+c);
+if (!isNaN(c)){
+    console.log("My number: "+ c);
 }
 else{
     console.log("Not a number");
