8f4cf2dd26d402c519458bd6e39fcd25f1c6e8d6
Etsub Berihun <etsubberihun09@gmail.com>
2025-07-12 11:32:40 +0300

    Task 7 was confusing too

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,11 @@
-const arg1=process.argv[2];
-const arg=parseInt(arg1);
-if(!isNaN(arg)){
-    for(let i=0;i<arg;i++){
-        console.log("C is fun");
-    }
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
 }
-else{
-console.log("Missing number of occurrences");
-}
+
 
